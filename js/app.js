5f85e1c37247c627b8010f55be3226cc4f7f1b22
Jackie605 <jackey605@hotmail.com>
2017-07-04 10:25:30 +0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,87 @@
+// 这是我们的玩家要躲避的敌人 
+var Enemy = function(loc) {
+    this.y=loc;// 要应用到每个敌人的实例的变量写在这里
+    this.x=-5;// 我们已经提供了一个来帮助你实现更多
+
+    this.speed=Math.random()*100;// 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
+    this.sprite = 'images/enemy-bug.png';
+    
+};
+
+// 此为游戏必须的函数，用来更新敌人的位置
+// 参数: dt ，表示时间间隙
+Enemy.prototype.update = function(dt) {
+    if(this.x>400){
+        this.x=-5;
+    }
+    this.x+=this.speed*dt;// 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
+    
+    // 都是以同样的速度运行的
+};
+
+// 此为游戏必须的函数，用来在屏幕上画出敌人，
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+Enemy.prototype.checkCollisions=function(player){
+    if(this.y==player.y){
+        if(Math.abs(this.x-player.x<=40)) {
+            player.x=200;
+            player.y=400;
+        }
+    }
+}
+
+
+var Player=function(x,y){
+    this.x=x;
+    this.y=y;
+    this.sprite='images/char-boy.png';
+
+};
+Player.prototype.update=function(){
+
+};
+Player.prototype.render=function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+Player.prototype.handleInput=function(direction){
+    switch(direction){
+        case 'left':if (this.x>0) 
+        {this.x-=100;}break;
+        case 'right':if (this.x<400) 
+        {this.x+=100;}break;
+        case 'up':if (this.y>28)
+        {this.y-=82;}break;
+        case 'down':if (this.y<400)
+        {this.y+=82;}break;
+    }
+    
+};
+// 现在实现你自己的玩家类
+// 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
+
+
+var enemy1=new Enemy(72);
+var enemy2=new Enemy(154);
+var enemy3=new Enemy(236);
+var enemy4=new Enemy(318);
+var player=new Player(200,400);// 现在实例化你的所有对象
+var allEnemies=[enemy1,enemy2,enemy3,enemy4];// 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
+for(var i=0;i<4;i++){
+    allEnemies[i].checkCollisions(player);
+}// 把玩家对象放进一个叫 player 的变量里面
+
+// 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Play.handleInput()
+// 方法里面。你不需要再更改这段代码了。
+// 
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
